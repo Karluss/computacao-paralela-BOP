9eeed4f6538c85ea04cc71c4bed51c8b14b65c14
Carlos de Oliveira <carlosmanoelba@hotmail.com>
2022-03-02 13:07:42 -0300

    LAB02

---
+++ lab02/lab02.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+   pid_t pid;
+   if (argc != 2)
+   {
+        printf("arg missing or exceeding\n");
+        exit(0);
+   }
+  
+    // atoi converts string to integer
+    if ( atoi ( argv[1] ) <0 )
+    {
+        printf("negative number entered %d", atoi(argv[1]));
+        exit(0);
+    }
+  
+    pid=fork();
+  
+    if ( pid<0 )
+    {
+        printf("failed to create child\n");
+        exit(0);
+    }
+    else if (pid == 0){
+
+        int i, num, j;
+        //printf ("Enter the number: ");
+        //scanf ("%d", &num);
+	num = atoi(argv[1]);
+        j = num;
+
+        for (i=1; j > 1; j = j -1) {
+             i=i*j;
+        }
+
+        printf("The factorial of %d is %d\n",num,i);
+	exit(0);
+   }
+
+    // parent process
+    else
+    {
+        wait(NULL);
+  
+        // waiting for child process to end
+        printf("Done\n");
+    }
+}
