237d0242da211224eedc9b63cdd32e36b27ed6e1
pedrobarretto <pedro@barretto.com.br>
2022-04-19 20:46:58 -0300

    feat: create logic for sum and multiplicity on both process

--- lab04/lab04.c
+++ lab04/lab04.c
@@ -13,7 +13,7 @@ int main()
 
     shmid = shmget(IPC_PRIVATE, 4 * sizeof(int), IPC_CREAT | 0666);
 
-    if ((shmid = shmget(IPC_PRIVATE, 4 * sizeof(int), IPC_CREAT | 0666) == -1))
+    if ((shmid < 0))
     {
         perror("Erro - SHMEGT");
         exit(1);
@@ -26,6 +26,10 @@ int main()
         exit(1);
     }
 
+    shmPTR[0] = 1;
+    printf("Server has saved %d in shared memory\n", shmPTR[0]);
+
+    printf("Server is about to fork a child process...\n");
     pid = fork();
 
     if (pid < 0)
@@ -35,13 +39,29 @@ int main()
     }
     else if (pid == 0)
     {
+        printf("   Processo filho encontrou na memoria compartilhada o valor %d\n", shmPTR[0]);
+        printf("   Processo filho vai realizar uma operacao de soma\n");
+        printf("   %d + 2 = %d\n", shmPTR[0], shmPTR[0] + 2);
+        printf("   Processo filho vai atualizar o valor na memoria compartilhada\n");
+        shmPTR[0] = shmPTR[0] + 2;
+        printf("   Processo filho atualizou o valor na memoria compartilhada para %d\n", shmPTR[0]);
+        printf("   Processo filho vai desconectar da memoria compartilhada\n");
+        printf("   Processo filho vai desalocar a memoria compartilhada\n");
+        printf("   Processo filho vai finalizar...\n");
         exit(0);
     }
-
     else
     {
         wait(NULL);
-        printf("\n%d\n", pid);
-        printf("Done\n");
+        printf("   Processo pai encontrou na memoria compartilhada o valor %d\n", shmPTR[0]);
+        printf("   Processo pai vai realizar uma operacao de multiplicao\n");
+        printf("   %d * 4 = %d\n", shmPTR[0], shmPTR[0] * 4);
+        printf("   Processo pai vai atualizar o valor na memoria compartilhada\n");
+        shmPTR[0] = shmPTR[0] * 4;
+        printf("   Processo pai atualizou o valor na memoria compartilhada para %d\n", shmPTR[0]);
+        printf("   Processo pai vai desconectar da memoria compartilhada\n");
+        printf("   Processo pai vai desalocar a memoria compartilhada\n");
+        printf("   Processo pai vai finalizar...\n");
+        exit(0);
     }
 }
