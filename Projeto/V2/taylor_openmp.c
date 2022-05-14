5f38c8e81a79a18ff4438a4eca9be0da49d45236
Carlos de Oliveira <carlosmanoelba@hotmail.com>
2022-05-14 13:31:56 -0300

    adding V2

---
+++ Projeto/V2/taylor_openmp.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int thread_count;
+double sum = 0;
+
+void *Taylor(void *rank);
+
+int main(int argc, char *argv[])
+{
+
+  long thread;
+  pthread_t *thread_handles;
+
+  thread_count = strtol(argv[1], NULL, 10);
+
+  thread_handles = malloc(thread_count * sizeof(pthread_t));
+
+  for (thread = 0; thread < thread_count; thread++)
+  {
+    pthread_create(&thread_handles[thread], NULL, Taylor, (void *)thread);
+  }
+
+  for (thread = 0; thread < thread_count; thread++)
+  {
+    pthread_join(thread_handles[thread], NULL);
+  }
+
+  free(thread_handles);
+
+  return 0;
+};
+
+void *Taylor(void *rank)
+{
+  long my_rank = (long)rank;
+  int num = (my_rank * (100 / thread_count)) + 1;
+  int condition = num + (100 / thread_count);
+
+  for (double i = num; i < condition; i++)
+  {
+    sum += 1 / i;
+  }
+
+  printf("Resultado: %.2f da thread %ld\n", sum, my_rank);
+
+  return NULL;
+}
