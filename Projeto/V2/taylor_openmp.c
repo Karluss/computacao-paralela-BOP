093e17ba577e95fc9a964e191fddc24722068fe2
Carlos de Oliveira <carlosmanoelba@hotmail.com>
2022-05-14 13:56:46 -0300

    V2 using OpenMP

--- Projeto/V2/taylor_openmp.c
+++ Projeto/V2/taylor_openmp.c
@@ -1,49 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <omp.h>
+
+#define T 100
 
 int thread_count;
-double sum = 0;
 
-void *Taylor(void *rank);
+void Taylor(double* global_result_p);
 
 int main(int argc, char *argv[])
 {
-
-  long thread;
-  pthread_t *thread_handles;
+  double global_result = 0.0;
 
   thread_count = strtol(argv[1], NULL, 10);
 
-  thread_handles = malloc(thread_count * sizeof(pthread_t));
+  # pragma omp parallel num_threads (thread_count)
+  Taylor(&global_result);
 
-  for (thread = 0; thread < thread_count; thread++)
-  {
-    pthread_create(&thread_handles[thread], NULL, Taylor, (void *)thread);
-  }
+  printf("Resultado: %.4f\n", global_result);
 
-  for (thread = 0; thread < thread_count; thread++)
-  {
-    pthread_join(thread_handles[thread], NULL);
-  }
-
-  free(thread_handles);
 
   return 0;
 };
 
-void *Taylor(void *rank)
+void Taylor(double* global_result_p)
 {
-  long my_rank = (long)rank;
-  int num = (my_rank * (100 / thread_count)) + 1;
-  int condition = num + (100 / thread_count);
+  long my_rank = omp_get_thread_num();
+  int num = (my_rank * ( T / thread_count)) + 1;
+  int condition = num + (T / thread_count);
+  float sum;
 
   for (double i = num; i < condition; i++)
   {
     sum += 1 / i;
   }
 
-  printf("Resultado: %.2f da thread %ld\n", sum, my_rank);
+  # pragma omp critical
+  *global_result_p += sum;
 
-  return NULL;
+
 }
