537ae006c3e145789b942f2fefc7e9c63504b61a
Carlos de Oliveira <carlosmanoelba@hotmail.com>
2022-05-24 22:19:19 -0300

    adding parallel for

--- Projeto/Final/taylor_reduction.c
+++ Projeto/Final/taylor_reduction.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <omp.h>
 
-#define T 1000000000
+#define T 100000000000
 
-double Taylor(double* global_result_p);
+double Taylor(double* global_result_p, int thread_count);
 
 int main(int argc, char *argv[])
 {
@@ -13,31 +13,35 @@ int main(int argc, char *argv[])
 
   thread_count = strtol(argv[1], NULL, 10);
 
-  # pragma omp parallel num_threads (thread_count)
-  Taylor(&global_result);
+  //# pragma omp parallel num_threads (thread_count) 
+  //reduction(+:global_result)
 
-  printf("Resultado: %.4f\n", global_result);
+  Taylor(&global_result, thread_count);
+
+  //printf("Resultado: %.4f\n", global_result);
 
 
   return 0;
 };
 
-double Taylor(double* global_result_p)
+double Taylor(double* global_result_p, int thread_count)
 {
-  long my_rank = omp_get_thread_num();
-  int thread_count = omp_get_num_threads();
-  int num = (my_rank * ( T / thread_count)) + 1;
-  int condition = num + (T / thread_count);
+  //long my_rank = omp_get_thread_num();
+  //int thread_count = omp_get_num_threads();
+  //int num = (my_rank * ( T / thread_count)) + 1;
+  //int condition = num + (T / thread_count);
   double sum = 0.0;
 
-  # pragma omp parallel for num_threads(thread_count)
-    //reduction(+:sum);
+  #pragma omp parallel for num_threads(thread_count) \
+    reduction(+:sum)
 
-    for (double i = num; i < condition; i++)
-    {
-        sum += 1 / i;
-    }
+  for (long int i = 1; i < T; i++)
+   {
+        double den = i;
+        sum += 1 / den;
+   }
 
 
-  printf("Thread %ld => Resultado: %.4f\n", my_rank, sum);
+  printf("Resultado: %.4f\n", sum);
+  return sum;
 }
