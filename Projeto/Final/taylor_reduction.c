e0d1dce77256b71f526b396f3b93894ea0b8ad17
Carlos de Oliveira <carlosmanoelba@hotmail.com>
2022-05-29 12:36:29 -0300

    finalized version with parallel for

--- Projeto/Final/taylor_reduction.c
+++ Projeto/Final/taylor_reduction.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <omp.h>
 
-#define T 100000000000
+#define T 100000000000// 11 zeros
 
-double Taylor(double* global_result_p, int thread_count);
+void Taylor(double* global_result_p, int thread_count);
 
 int main(int argc, char *argv[])
 {
@@ -13,23 +13,13 @@ int main(int argc, char *argv[])
 
   thread_count = strtol(argv[1], NULL, 10);
 
-  //# pragma omp parallel num_threads (thread_count) 
-  //reduction(+:global_result)
-
   Taylor(&global_result, thread_count);
-
-  //printf("Resultado: %.4f\n", global_result);
-
 
   return 0;
 };
 
-double Taylor(double* global_result_p, int thread_count)
+void Taylor(double* global_result_p, int thread_count)
 {
-  //long my_rank = omp_get_thread_num();
-  //int thread_count = omp_get_num_threads();
-  //int num = (my_rank * ( T / thread_count)) + 1;
-  //int condition = num + (T / thread_count);
   double sum = 0.0;
 
   #pragma omp parallel for num_threads(thread_count) \
@@ -43,5 +33,4 @@ double Taylor(double* global_result_p, int thread_count)
 
 
   printf("Resultado: %.4f\n", sum);
-  return sum;
 }
---
+++ Projeto/Final/taylor_serial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define T  100000000000// 11 zeros
+
+int main(void)
+{
+  double var = 0;
+
+  for (double i = 1; i <= T; i++)
+  {
+    var += 1 / i;
+  }
+
+  printf("Resultado: %.4f\n", var);
+
+  return 0;
+}
